f2f185a2774e44d74158b00759bb94c607c57d9e
pablobarrado <pablobarrado@gmail.com>
2025-04-27 19:59:56 +0200

    palindromo con for

--- 01_desafios_texto/01_01_palindromo.py
+++ 01_desafios_texto/01_01_palindromo.py
@@ -4,6 +4,24 @@ def es_palindromo(texto):
     texto_sin_espacios = texto_minuscula.replace(" ", "")
     return texto_sin_espacios == texto_sin_espacios[::-1]
 
+def es_palindromo2(texto):
+    
+    texto = texto.lower().replace(" ", "")
+    print(texto)
+    j = len(texto)-1
+    for i in texto:
+     
+     if (i != texto[j]):
+        print(f"{i} y {texto[j]}")
+        return False
+     j=j-1
+     
+    return True    
+        
 
+'''
 print(es_palindromo("Anita lava la tina"))  # True
 print(es_palindromo("palindromo"))  # False
+'''
+
+print(es_palindromo2("Anita lava la tina"))
